c2c5736bfd9fbe164950d12739f0cfe9ab2c3a06
Michael Freno <michaelt.freno@gmail.com>
2026-01-07 23:28:17 -0500

    Initial Commit

---
+++ Gaze/GazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GazeApp.swift
+//  Gaze
+//
+//  Created by Mike Freno on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct GazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GazeTests/GazeTests.swift
@@ -0,0 +1,17 @@
+//
+//  GazeTests.swift
+//  GazeTests
+//
+//  Created by Mike Freno on 1/7/26.
+//
+
+import Testing
+@testable import Gaze
+
+struct GazeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
